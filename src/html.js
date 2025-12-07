de885c8da595bbfb3c545deb33e6a2a690e6e9c3
Your Name <you@example.com>
2025-12-07 03:32:54 +0300

    Fix mobile navigation positioning with safe areas and viewport meta tag

---
+++ src/html.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+        />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  )
+}
