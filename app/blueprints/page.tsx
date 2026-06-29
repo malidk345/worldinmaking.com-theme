dac4f8b7d05650a24a773be94d71d91430fa1cd4
malidk345 <dursunkayamustafa@gmail.com>
2026-06-30 01:20:48 +0300

    fix: use relative paths for components to resolve build issues in app router subdirectories

--- app/blueprints/page.tsx
+++ app/blueprints/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React from 'react'
-import Wrapper from 'components/Wrapper'
+import Wrapper from '../../../components/Wrapper'
 
 export default function Home() {
     return (
